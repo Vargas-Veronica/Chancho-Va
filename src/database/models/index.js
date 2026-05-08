819ab16ab492114115c5d92d605624bf42c857d8
Vargas-Veronica <vargas.veronica55@gmail.com>
2026-05-08 18:22:24 -0300

    fix: update Sequelize initialization to use DATABASE_URL environment variable

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -12,12 +12,8 @@ require('dotenv').config();
 const db = {};
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false
   }
