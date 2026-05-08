6cbcea88a84d9334cf0c8543109e0e018413b68a
Vargas-Veronica <vargas.veronica55@gmail.com>
2026-05-08 18:01:42 -0300

    fix: refactor database connection to use MySQL configuration

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -4,23 +4,25 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+
+require('dotenv').config();
+
 const db = {};
 
-/*let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}*/
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: { require: true, rejectUnauthorized: false }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false
   }
-});
+);
+
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -44,7 +46,5 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-
 
 module.exports = db;
